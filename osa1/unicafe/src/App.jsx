30bf20a11dd68d947a26ae2ee6260efb837d7b3c
160cmAgresor <roni.raja@edu.hyria.fi>
2023-11-28 11:11:03 +0200

    Unicafe step 4 kesken

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+<button onClick={props.handleClick}> {props.text} </button>
+)
+
+const Statistics = (props) => {
+  return (
+  <p>{props.text} {props.value}</p>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [all, setAll] = useState(0)
+  const [pos, setPos] = useState(0)
+
+  const increaseGood = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setAll(all + 1)
+    setPos(pos + 1)
+    setTotal(bad + neutral + updatedGood)
+  }
+
+  const increaseBad = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setAll(all - 1)
+    setTotal(good + neutral + updatedBad)
+  }
+
+  const increaseNeutral = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setAll(all + 0)
+    setTotal(good + bad + updatedNeutral)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+
+      <Button handleClick={increaseGood} text="good" />
+      <Button handleClick={increaseNeutral} text="neutral" />
+      <Button handleClick={increaseBad} text="bad" />
+
+      <h1>statistics</h1>
+
+      <Statistics text="good" value={good} />
+      <Statistics text="neutral" value={neutral} />
+      <Statistics text="bad" value={bad} />
+      <Statistics text="all" value={total} />
+      <Statistics text="average" value={all / total} />
+      <Statistics text="positive" value={(pos / total) * 100 + " %"} />
+    </div>
+  )
+}
+
+export default App
